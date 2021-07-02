ecb24ebd974ec82f220312f2b8413a182f5a53c0
davidebortoloni <davidebortoloni@gmail.com>
2021-07-02 17:13:18 +0200

    Gioco dei dadi [esercizio finito]

--- js/script-dadi.js
+++ js/script-dadi.js
@@ -1,5 +1,42 @@
-/*
-GIOCO DEI DADI
-Generare un numero random da 1 a 6 sia per il giocatore sia per il computer.
-Stabilire il vincitore, in base a chi fa il punteggio più alto e stampare il risultato.
-*/
+alert("Quando sei pronto per giocare clicca ok");
+
+var userResult;
+var pcResult;
+var userVictories = 0;
+var pcVictories = 0;
+var gameMessage = "";
+var endMessage;
+var gameAreaId = document.getElementById("game-area");
+var endGameId = document.getElementById("end-game");
+
+for (var i = 0; i < 5; i++) {
+    userResult = Math.floor(Math.random() * 6) + 1;
+    pcResult = Math.floor(Math.random() * 6) + 1;
+    gameMessage += "<h3>" + (i + 1) + "° round</h3>";
+    gameMessage += "<div>Il tuo dado: " + userResult + "</div>";
+    gameMessage += "<div>Il dado del computer: " + pcResult + "</div>";
+    if (userResult > pcResult) {
+        gameMessage += "<div>Bravo! Hai vinto il " + (i + 1) + "° round</div>";
+        userVictories++;
+    }
+    else if (userResult < pcResult) {
+        gameMessage += "<div>Peccato! Hai perso il " + (i + 1) + "° round</div>";
+        pcVictories++;
+    }
+    else {
+        gameMessage += "<div>Nulla di fatto! Il " + (i + 1) + "° round è un pareggio</div>";
+    }
+    gameAreaId.innerHTML = gameMessage;
+}
+
+if (userVictories > pcVictories) {
+    endMessage = "Complimenti, hai battuto il computer!";
+}
+else if (userVictories < pcVictories) {
+    endMessage = "Che sfortuna, il computer ti ha sconfitto!";
+}
+else {
+    endMessage = "Un pareggio, meglio che aver perso, dai. Fai un altro tentativo.";
+}
+
+endGameId.innerHTML = endMessage;
